93f26cf4015266832121ad361280198ff3efe725
Mike <mike@tigerblue.io>
2023-08-27 22:51:50 +0100

    Added support for correct date format

--- csv2tcx.py
+++ csv2tcx.py
@@ -1,5 +1,14 @@
 import csv
 from xml.etree.ElementTree import Element, SubElement, tostring, ElementTree
+from datetime import datetime, timedelta
+
+# Function to calculate the date of the workout
+def calculate_date(start_date, week, day):
+    days_from_start = (int(week) - 1) * 7 + int(day) - 1
+    return start_date + timedelta(days=days_from_start)
+
+# Initialize start date
+start_date = datetime(2023, 8, 28)
 
 # Create the root element
 root = Element('TrainingCenterDatabase')
@@ -10,13 +19,16 @@ with open('training_plan.csv', 'r') as csvfile:
     next(reader)  # Skip header
     for row in reader:
         week, day, workout, duration, distance, details, intensity, equipment, description, notes = row
+
+        # Calculate the date for this workout
+        workout_date = calculate_date(start_date, week, day)
+
         # Create Activity element
         activity = SubElement(root, 'Activity')
 
         # Create elements for each data point and add them to the Activity
-        SubElement(activity, 'Week').text = week
-        SubElement(activity, 'Day').text = day
-        SubElement(activity, 'Workout').text = workout
+        SubElement(activity, 'Date').text = workout_date.strftime('%Y-%m-%d')
+        SubElement(activity, 'WorkoutName').text = f"{workout} - {workout_date.strftime('%Y-%m-%d')}"
         SubElement(activity, 'Duration').text = duration
         SubElement(activity, 'Distance').text = distance
         SubElement(activity, 'Details').text = details
@@ -24,6 +36,7 @@ with open('training_plan.csv', 'r') as csvfile:
         SubElement(activity, 'Equipment').text = equipment
         SubElement(activity, 'Description').text = description
         SubElement(activity, 'Notes').text = notes
+
 # Create the XML file
 tree = ElementTree(root)
 with open('training_plan.tcx', 'wb') as f:
